52322aed4a5df525c787a61982b28f2aab6571ee
Yusuf G <youssefgh555@hotmail.com>
2024-07-07 15:34:45 +0000

    done

--- myApp.js
+++ myApp.js
@@ -1,9 +1,17 @@
 let express = require('express');
 let app = express();
 
+/*
+task 2
 app.get('/', (req, res) => {
     res.send('Hello Express');
 });
+*/
+
+// task 3
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + "/views/index.html");
+  });
 
 //console.log("Hello World")
 
